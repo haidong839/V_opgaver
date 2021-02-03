f772709360ab5a849716e57c43ccf33e18766960
Mohamud Abdirahman <mohamud_new258@hotmail.com>
2021-02-03 14:22:24 +0100

    Rohan og Jeg har lavet javadoc på "GameController"

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -53,6 +53,12 @@ public class GameController {
         //   - the counter of moves in the game should be increased by one
         //     if the player is moved
 
+       /**
+         * Den aktuelle spiller kaldes til brættet.
+         * Antallet af felter, man har rykket frem registrers
+         * Kalder og videregiver oplysninger til spillerens nummer og derefter næste spillers tur
+         */
+
         Player currentPlayer = board.getCurrentPlayer();
 
         if (space.getPlayer() == null){
@@ -75,6 +81,12 @@ public class GameController {
         //     is not yet implemented
     }
 
+    /**
+     * Denne funktion
+     * @param source
+     * @param target
+     * @return
+     */
     public boolean moveCards(@NotNull CommandCardField source, @NotNull CommandCardField target) {
         CommandCard sourceCard = source.getCard();
         CommandCard targetCard = target.getCard();
